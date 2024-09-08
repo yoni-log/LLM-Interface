59fd4abb828485ab041a9060ef832fe8f5973658
ioanni <ioannisgiannopoulosloyola@gmail.com>
2024-09-07 19:30:33 -0500

    added model selection

--- converse.rs
+++ converse.rs
@@ -3,12 +3,14 @@ use crate::python;
 
 pub struct Conversations {
     pub conversations: Vec<(String, String)>,
+    pub model: String,
 }
 
 impl Conversations  {
     pub fn new() -> Self {
         Self {
             conversations: Vec::new(),
+            model: "Input your model here!".to_string(),
         }
     }
 
@@ -24,14 +26,14 @@ impl Conversations  {
     }
 
     pub fn send_input(conversations: &mut Conversations, input: &str) -> bool {
-        let input_and_output = Self::get_output(input);
+        let input_and_output = Self::get_output(conversations.model.as_str(), input);
         let gpt_output = input_and_output.split_at(input.len() + 1).1;
         conversations.push_conversation(input, gpt_output);
         true
     }
 
-    pub fn get_output(input: &str) -> String {
-        match python::get_llm_output(input.as_str()) {
+    pub fn get_output(model: &str, input: &str) -> String {
+        match python::get_llm_output(input.as_str(), model.as_str()) {
             Ok(out) => out.to_string(),
             Err(E) => format!("{input}-Error: {E}").to_string()
         }
